d632267572aa62c33a55126a38f6a9bffb8ddae0
stop4uk <stop4uk@yandex.ru>
2024-07-08 23:07:59 +0300

    Update main.php
    Фикс генерации Toast плагина при получении ошибки 403

--- resources/views/layouts/main.php
+++ resources/views/layouts/main.php
@@ -6,12 +6,23 @@
  */
 
 use yii\helpers\Json;
-use yii\bootstrap5\Html;
-use yii\bootstrap5\Breadcrumbs;
+use yii\bootstrap5\{
+    Html,
+    Breadcrumbs
+};
 use app\assets\MainAsset;
 use app\widgets\AlertToast;
 
 MainAsset::register($this);
+
+$messages = Json::htmlEncode([
+    'forbiddenTemplate' => Yii::t('exceptions', 'У Вас отсутствуют права доступа для осуществления данного действия. Пожалуйста, обратитесь к администратору')
+]);
+
+$this->registerJs(<<< JS
+    var langMessages = $messages;
+JS, $this::POS_BEGIN, 'langMessages');
+
 
 $this->registerCsrfMetaTags();
 $this->registerMetaTag(['charset' => Yii::$app->charset], 'charset');
@@ -21,86 +32,77 @@ $this->registerMetaTag(['name' => 'keywords', 'content' => Yii::$app->settings->
 $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii::getAlias('@web/favicon.ico')]);
 ?>
 <?php $this->beginPage() ?>
-<!DOCTYPE html>
+    <!DOCTYPE html>
     <html lang="<?= Yii::$app->language ?>" class="h-100">
-        <head>
-            <title><?= Html::encode(Yii::$app->settings->get('system', 'app_name')) ?></title>
-            <?php $this->head() ?>
-        </head>
-        <body class="d-flex flex-column vh-100">
-        <?php $this->beginBody() ?>
-            <?= AlertToast::widget() ?>
-            <div id="loadingData" class="text-center">
-                <div class="spinner-border text-light" role="status">
-                    <span class="visually-hidden"><?= Yii::t('views', 'Загрузка'); ?></span>
+    <head>
+        <title><?= Html::encode(Yii::$app->settings->get('system', 'app_name')) ?></title>
+        <?php $this->head() ?>
+    </head>
+    <body class="d-flex flex-column vh-100">
+    <?php $this->beginBody() ?>
+    <?= AlertToast::widget() ?>
+    <div id="loadingData" class="text-center">
+        <div class="spinner-border text-light" role="status">
+            <span class="visually-hidden"><?= Yii::t('views', 'Загрузка'); ?></span>
+        </div>
+    </div>
+    <div id="hidescreen"></div>
+    <div class="position-relative" aria-atomic="true" aria-live="polite">
+        <div class="toast-container position-absolute top-0 end-0 p-3">
+            <div class="text-white" id="mainToast" role="alert" aria-live="assertive" aria-atomic="true">
+                <div class="d-flex">
+                    <div class="toast-body" id="mainToastBody"></div>
+                    <button class="me-2 m-auto btn-close d-none" type="button" data-bs-dismiss="toast" aria-label="<?= Yii::t('views', 'Закрыть'); ?>" id="btn_closeMainToast">
                 </div>
             </div>
-            <div id="hidescreen"></div>
-            <div class="position-relative" aria-atomic="true" aria-live="polite">
-                <div class="toast-container position-absolute top-0 end-0 p-3">
-                    <div class="text-white" id="mainToast" role="alert" aria-live="assertive" aria-atomic="true">
-                        <div class="d-flex">
-                            <div class="toast-body" id="mainToastBody"></div>
-                            <button class="me-2 m-auto btn-close d-none" type="button" data-bs-dismiss="toast" aria-label="<?= Yii::t('views', 'Закрыть'); ?>" id="btn_closeMainToast">
+        </div>
+    </div>
+
+    <div class="wrapper">
+        <?= $this->render('_blocks/main_panel_left'); ?>
+        <div class="main">
+            <?= $this->render('_blocks/main_panel_top'); ?>
+            <main class="content">
+                <div class="container-fluid p-0">
+                    <div class="row mb-3">
+                        <div class="col-12 col-md-5">
+                            <?php if ($this->title): ?>
+                                <h3 class="d-table-cell"><strong><?= $this->title;?></strong></h3>
+                            <?php endif; ?>
                         </div>
-                    </div>
-                </div>
-            </div>
 
-            <div class="wrapper">
-                <?= $this->render('_blocks/main_panel_left'); ?>
-                <div class="main">
-                    <?= $this->render('_blocks/main_panel_top'); ?>
-                    <main class="content">
-                        <div class="container-fluid p-0">
-                            <div class="row mb-3">
-                                <div class="col-12 col-md-5">
-                                    <?php if ($this->title): ?>
-                                        <h3 class="d-table-cell"><strong><?= $this->title;?></strong></h3>
-                                    <?php endif; ?>
-                                </div>
-
-                                <?php if ( isset($this->params['breadcrumbs']) ): ?>
-                                    <div class="<?= !$this->title ? 'col-md-12' : 'col-md-7' ?> d-none d-md-flex justify-content-end">
-                                        <?= Breadcrumbs::widget([
-                                            'options' => ['class' => 'mb-1 mt-1'],
-                                            'homeLink' => ['label' => Yii::t('modules', 'Главная'), 'url' => '/'],
-                                            'links' => $this->params['breadcrumbs'],
-                                        ]) ?>
-                                    </div>
-                                <?php endif; ?>
+                        <?php if ( isset($this->params['breadcrumbs']) ): ?>
+                            <div class="<?= !$this->title ? 'col-md-12' : 'col-md-7' ?> d-none d-md-flex justify-content-end">
+                                <?= Breadcrumbs::widget([
+                                    'options' => ['class' => 'mb-1 mt-1'],
+                                    'homeLink' => ['label' => Yii::t('modules', 'Главная'), 'url' => '/'],
+                                    'links' => $this->params['breadcrumbs'],
+                                ]) ?>
                             </div>
-
-                            <?= $content; ?>
-                        </div>
-                    </main>
-
-                    <?= $this->render('_blocks/footer'); ?>
-                </div>
-            </div>
-
-            <div class="modal" tabindex="-1" id="modalWindow">
-                <div class="modal-dialog" id="modalWindow_dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <div id="modalWindow_header"></div>
-                            <button id="modalWindow_closeButton" type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div id="modalWindow_content" class="modal-body"></div>
+                        <?php endif; ?>
                     </div>
+
+                    <?= $content; ?>
                 </div>
+            </main>
+
+            <?= $this->render('_blocks/footer'); ?>
+        </div>
+    </div>
+
+    <div class="modal" tabindex="-1" id="modalWindow">
+        <div class="modal-dialog" id="modalWindow_dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <div id="modalWindow_header"></div>
+                    <button id="modalWindow_closeButton" type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div id="modalWindow_content" class="modal-body"></div>
             </div>
-        <?php $this->endBody() ?>
-        </body>
+        </div>
+    </div>
+    <?php $this->endBody() ?>
+    </body>
     </html>
 <?php
 $this->endPage();
-
-$messages = Json::htmlEncode([
-    'forbiddenTemplate' => Yii::t('exceptions', 'У Вас отсутствуют права доступа для осуществления данного действия. Пожалуйста, обратитесь к администратору')
-]);
-
-$this->registerJs(<<< JS
-    var langMessages = $messages
-JS, $this::POS_HEAD, 'langMessages');
-
