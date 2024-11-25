f9d3f064164025abedc61cad5315ca4495cce675
stop4uk <stop4uk@yandex.ru>
2024-11-25 19:35:28 +0300

    Update index.php
    View modification

--- application/resources/views/reports/constant/index.php
+++ application/resources/views/reports/constant/index.php
@@ -1,22 +1,24 @@
 <?php
 
+use yii\helpers\Url;
+use yii\widgets\Pjax;
+use yii\grid\ActionColumn;
+use yii\bootstrap5\Html;
+
+use app\widgets\GridView;
+use app\helpers\{
+    CommonHelper,
+    RbacHelper
+};
+
 /**
  * @var \app\search\report\ConstantSearch $searchModel
  * @var \yii\data\ActiveDataProvider $dataProvider
  */
 
-use yii\widgets\{
-    Pjax,
-    ListView
-};
-use yii\bootstrap5\Html;
-
-use app\widgets\Pager;
-
 $this->title = Yii::t('views', 'Список констант');
 
 ?>
-
     <div class="d-flex justify-content-end mb-2">
         <?php
             if ( Yii::$app->getUser()->can('constant.create') ) {
@@ -33,9 +35,150 @@ $this->title = Yii::t('views', 'Список констант');
         ?>
     </div>
 
-<?php
-    Pjax::begin(['id' => 'constantList', 'enablePushState' => false, 'clientOptions' => ['method' => 'POST']]);
-        echo $this->render('_partial/search', ['searchModel' => $searchModel]);
+<?php Pjax::begin(['id' => 'constantList', 'enablePushState' => false, 'clientOptions' => ['method' => 'POST']]); ?>
+    <?= $this->render('_partial/search', ['searchModel' => $searchModel]); ?>
+    <div class="card">
+        <div class="card-body">
+            <?=  GridView::widget([
+                'dataProvider' => $dataProvider,
+                'emptyText' => Yii::t('views', 'Отчеты отсутствуют'),
+                'tableOptions' => ['class' => 'table'],
+                'columns' => [
+                    [
+                        'attribute' => 'name',
+                        'headerOptions' => [
+                            'width' => '30%'
+                        ],
+                        'format' => 'raw',
+                        'value' => function($data) {
+                            $resultString = Html::tag('i', '', [
+                                'class' => 'bi bi-circle-fill me-2 text-' . CommonHelper::getYesOrNoRecordColor($data->record_status),
+                                'data-bs-toggle' => 'tooltip',
+                                'data-bs-placement' => 'bottom',
+                                'title' => Yii::t('views', 'Статус записи: {status}', ['status' => CommonHelper::getYesOrNoRecord($data->record_status)])
+                            ]);
+
+                            if($data->description) {
+                                $resultString .= Html::tag('i', '', [
+                                    'class' => 'me-2 bi bi-question-circle',
+                                    'tabindex' => 0,
+                                    'role' => 'button',
+                                    'title' => Yii::t('views', 'Описание'),
+                                    'data-bs-toggle' => 'popover',
+                                    'data-bs-content' => $data->description,
+                                    'data-bs-html' => 'true',
+                                    'data-bs-trigger' => 'focus'
+                                ]);
+                            }
+
+                            $name = $data->name;
+                            if ($data->name_full) {
+                                $name .= Html::tag('span', '#' . $data->name_full, ['class' => 'small text-muted ms-2']);
+                            }
+
+                            return $resultString . $name;
+                        }
+                    ],
+                    [
+                        'label' => null,
+                        'headerOptions' => [
+                            'width' => '8%'
+                        ],
+                        'format' => 'raw',
+                        'value' => function($data) {
+                            $resultString = '';
+
+                            if ( $data->reports_only ) {
+                                $resultString .= Html::tag('span', '<i class="bi bi-exclamation-triangle p-1 me-2"></i>', [
+                                    'class' => 'text-danger fw-bold',
+                                    'data-bs-toggle' => 'tooltip',
+                                    'data-bs-placement' => 'bottom',
+                                    'title' => Yii::t('views', 'Доступна только для {n, plural, =1{одного отчета} one{# отчета} few{# отчетов} many{# отчетов} other{# отчетов}}', ['n' => count(CommonHelper::explodeField($data->reports_only))]),
+                                ]);
+                            }
+
+                            if ( $data->union_rules ) {
+                                $resultString .= Html::tag('span', '<i class="bi bi-columns-gap p-1 me-2"></i>', [
+                                    'class' => 'text-primary fw-bold',
+                                    'data-bs-toggle' => 'tooltip',
+                                    'data-bs-placement' => 'bottom',
+                                    'title' => Yii::t('views', 'Правило группировки: {rule}', ['rule' => $data->union_rules]),
+                                ]);
+                            }
+
+                            return $resultString;
+                        }
+                    ],
+                    [
+                        'attribute' => 'created_at',
+                        'headerOptions' => [
+                            'width' => '20%'
+                        ],
+                        'format' => ['date', Yii::$app->settings->get('system', 'app_language_dateTime')],
+                    ],
+                    [
+                        'class' => ActionColumn::class,
+                        'header' => false,
+                        'headerOptions' => ['width' => '10%'],
+                        'contentOptions' => ['class' => 'text-center'],
+                        'template' => '{view} {edit} {delete}',
+                        'buttons' => [
+                            'view' => function($url, $model) {
+                                return Html::a('<i class="bi bi-eye text-dark"></i>', Url::to(['view', 'id' => $model->id]), ['data-pjax' => 0]);
+                            },
+                            'edit' => function($url, $model) {
+                                return Html::a('<i class="bi bi-pen text-dark"></i>', Url::to(['edit', 'id' => $model->id]), ['data-pjax' => 0]);
+                            },
+                            'delete' => function($url, $model) {
+                                return Html::tag('span', '<i class="bi bi-trash text-dark"></i>', [
+                                    'role' => 'button',
+                                    'data-message' => Yii::t('views', 'Вы действительно хотите удалить константу "{name}"?', ['name' =>  $model->name]),
+                                    'data-url' => Url::to(['delete', 'id' => $model->id]),
+                                    'data-pjaxContainer' => '#constantList',
+                                    'onclick' => 'workWithRecord($(this))',
+                                ]);
+                            }
+                        ],
+                        'visibleButtons' => [
+                            'view' => function($model) {
+                                $ruleArray = $model->toArray(['created_uid', 'created_gid', 'record_status']);
+                                $rolesArray = [
+                                    'constant.view.main',
+                                    'constant.view.group',
+                                    'constant.view.all',
+                                    'constant.view.delete.main',
+                                    'constant.view.delete.group',
+                                    'constant.view.delete.all'
+                                ];
+
+                                return RbacHelper::canArray($rolesArray, $ruleArray);
+                            },
+                            'edit' => function($model){
+                                $ruleArray = $model->toArray(['created_uid', 'created_gid', 'record_status']);
+                                $rolesArray = [
+                                    'constant.edit.main',
+                                    'constant.edit.group',
+                                    'constant.edit.all',
+                                ];
+
+                                return RbacHelper::canArray($rolesArray, $ruleArray);
+                            },
+                            'delete' => function($model){
+                                $ruleArray = $model->toArray(['created_uid', 'created_gid', 'record_status']);
+                                $rolesArray = [
+                                    'constant.delete.main',
+                                    'constant.delete.group',
+                                    'constant.delete.all',
+                                ];
+
+                                return RbacHelper::canArray($rolesArray, $ruleArray);
+                            }
+                        ]
+                    ],
+                ]
+            ]); ?>
+        </div>
+    </div>
         echo ListView::widget([
             'dataProvider' => $dataProvider,
             'layout' => '{items}',
@@ -48,6 +191,7 @@ $this->title = Yii::t('views', 'Список констант');
         echo Pager::widget([
             'pagination' => $dataProvider->getPagination(),
         ]);
+<?php
     Pjax::end();
 
 
