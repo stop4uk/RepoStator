b68f531116bcbf094224d4af43cc3e7d4c86df13
stop4uk <stop4uk@yandex.ru>
2024-11-28 12:05:58 +0300

    Update index.php
    Update send report template

--- src/resources/views/reports/send/index.php
+++ src/resources/views/reports/send/index.php
@@ -15,33 +15,33 @@ $this->title = Yii::t('views', 'Выбор отчета для передачи'
 ?>
 
 <?php Pjax::begin(['id' => 'reportWorkList', 'enablePushState' => false, 'clientOptions' => ['method' => 'POST']]) ?>
-<?= $this->render('_partial/search', ['searchModel' => $searchModel]); ?>
-
-<p class="text-muted text-justify small">
-    <i class="bi bi-info-circle text-primary fw-bold"></i> <?= Yii::t('views', 'Присутствуют только те отчеты, которые Вам доступны и, для которых возможна передача сведений. ' .
-    'Если, сведения уже переданы в текущий расчетный период времени (исходя из заданных ограничений) - отчет будет отсутствовать в данном ' .
-    'списке. При этом, если, у Вас есть права на осуществления контроля, Вы можете поглядеть все ранее переднные сведения по отчетам'); ?>
-</p>
-
-<div class="row">
-    <div class="col-12">
-        <?= ListView::widget([
-            'dataProvider' => $dataProvider,
-            'layout' => '{items}',
-            'emptyText' => Yii::t('views', 'Отчеты для передачи отсутствуют'),
-            'emptyTextOptions' => [
-                'class' => 'alert alert-success text-center fw-bold'
-            ],
-            'viewParams' => [
-                'groups' => $searchModel->groups
-            ],
-            'itemView' => '_partial/reportItemSelect',
-            'options' => ['class' => 'row'],
-            'itemOptions' => ['class' => 'col-12'],
-            'pager' => [
-                'class' => Pager::class
-            ]
-        ]); ?>
+    <?php if ($dataProvider->getTotalCount()): ?>
+        <?= $this->render('_partial/search', ['searchModel' => $searchModel]); ?>
+        <p class="text-muted text-justify small">
+            <i class="bi bi-info-circle text-primary fw-bold"></i> <?= Yii::t('views', 'Присутствуют только те отчеты, которые Вам доступны и, для которых возможна передача сведений. ' .
+            'Если, сведения уже переданы в текущий расчетный период времени (исходя из заданных ограничений) - отчет будет отсутствовать в данном ' .
+            'списке. При этом, если, у Вас есть права на осуществления контроля, Вы можете поглядеть все ранее переднные сведения по отчетам'); ?>
+        </p>
+    <?php endif; ?>
+    <div class="row">
+        <div class="col-12">
+            <?= ListView::widget([
+                'dataProvider' => $dataProvider,
+                'layout' => '{items}',
+                'emptyText' => Yii::t('views', 'Отчеты для передачи отсутствуют'),
+                'emptyTextOptions' => [
+                    'class' => 'alert alert-success text-center fw-bold'
+                ],
+                'viewParams' => [
+                    'groups' => $searchModel->groups
+                ],
+                'itemView' => '_partial/reportItemSelect',
+                'options' => ['class' => 'row'],
+                'itemOptions' => ['class' => 'col-12'],
+                'pager' => [
+                    'class' => Pager::class
+                ]
+            ]); ?>
+        </div>
     </div>
-</div>
 <?php Pjax::end();
