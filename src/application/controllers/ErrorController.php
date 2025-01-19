ec95afaf4d799a57f56f019876551f0ae3cd701f
stop4uk <stop4uk@yandex.ru>
2025-01-19 19:23:38 +0300

    SystemControllers

---
+++ src/application/controllers/ErrorController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace app\controllers;
+
+use yii\web\Controller;
+
+/**
+ * @author Stop4uk <stop4uk@yandex.ru>
+ * @package app\controllers
+ */
+final class ErrorController extends Controller
+{
+    public function actionFault(): string|bool
+    {
+        $exception = Yii::$app->errorHandler->exception;
+
+        if ( $exception !== null ) {
+            $statusCode = $exception->statusCode ?? 999;
+            $name = $exception->getName();
+            $message = $exception->getMessage();
+            $page = ( !in_array($statusCode, ['403', '404']) ) ? 'other' : $statusCode;
+
+            return $this->render($page, compact('exception', 'statusCode', 'name', 'message'));
+        }
+
+        return false;
+    }
+}
---
+++ src/application/controllers/OfflineController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace app\controllers;
+
+use yii\web\Controller;
+
+/**
+ * @author Stop4uk <stop4uk@yandex.ru>
+ * @package app\controllers
+ */
+final class OfflineController extends Controller
+{
+    public function beforeAction($action): bool
+    {
+        if ( !Yii::$app->settings->get('system', 'app_maintenance') ) {
+            $this->goHome();
+        }
+
+        return parent::beforeAction($action);
+    }
+
+    public function actionIndex(): string
+    {
+        return $this->render('index');
+    }
+}
