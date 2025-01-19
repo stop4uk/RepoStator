487b1e38031600c56cb4e2e38db1a8c0463e06d4
stop4uk <stop4uk@yandex.ru>
2025-01-19 19:24:37 +0300

    run function

--- src/application/modules/users/UserModule.php
+++ src/application/modules/users/UserModule.php
@@ -2,12 +2,64 @@
 
 namespace app\modules\users;
 
-use yii\base\Module;
+use app\modules\users\entities\UserEntity;
+use Yii;
+use yii\base\{
+    Event,
+    Module
+};
+use yii\web\Application;
+
+use app\modules\users\components\{
+    Identity,
+    RbacDbmanager
+};
+use yii\web\User;
 
 final class UserModule extends Module
 {
-    public function init(): void
+    public static function run(): void
     {
-        parent::init();
+        $configPath = dirname(__FILE__, 1) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR;
+        $eventsFilePath = $configPath . 'events.php';
+        $routesFilePath = $configPath . 'routes.php';
+        $logsFilePath = $configPath . match((bool)env('YII_DEBUG')) {
+            true => 'logs_file.php',
+            false => 'logs_db.php'
+        };
+
+        if (Yii::$app instanceof Application) {
+            Yii::$app->setComponents([
+                'user' => [
+                    'class' => User::class,
+                    'identityClass' => Identity::class,
+                    'enableAutoLogin' => true,
+                    'loginUrl' => ['login'],
+                    'identityCookie' => [
+                        'name' => '_identity-' . env('PROJECT_NAME', 'simple'),
+                    ],
+                ],
+                'authManager' => RbacDbmanager::class
+            ]);
+        }
+
+        if (file_exists($routesFilePath)) {
+            $routes = require_once $routesFilePath;
+            Yii::$app->getUrlManager()->addRules($routes);
+        }
+
+        if (file_exists($logsFilePath)) {
+            $logs = require_once $logsFilePath;
+            foreach ($logs as $log) {
+                Yii::$app->getLog()->targets[] = $log;
+            }
+        }
+
+        if (file_exists($eventsFilePath)) {
+            $events = require_once $eventsFilePath;
+            foreach ($events as $event) {
+                Event::on($event['class'], $event['event'], $event['callable']);
+            }
+        }
     }
 }
