dfebe8dd172c8b5079f505ff94cc9e9fe13b2a2b
stop4uk <stop4uk@yandex.ru>
2025-01-19 20:11:09 +0300

    Fix user module logs

--- src/application/modules/users/config/logs_db.php
+++ src/application/modules/users/config/logs_db.php
@@ -1,60 +1,51 @@
 <?php
 
-use yii\log\DbTarget;
 use app\entities\LogEntity;
 
 return [
     [
-        'class' => DbTarget::class,
         'logTable' => LogEntity::tableName(),
         'categories' => ['Auth.Signin'],
         'levels' => ['error'],
         'logVars' => ['_POST'],
     ],
     [
-        'class' => DbTarget::class,
         'logTable' => LogEntity::tableName(),
         'categories' => ['Auth.Signup'],
         'levels' => ['error'],
         'logVars' => ['_POST'],
     ],
     [
-        'class' => DbTarget::class,
         'logTable' => LogEntity::tableName(),
         'categories' => ['Auth.Verification'],
         'levels' => ['error'],
         'logVars' => ['_GET'],
     ],
     [
-        'class' => DbTarget::class,
         'logTable' => LogEntity::tableName(),
         'categories' => ['Auth.Recovery'],
         'levels' => ['error'],
         'logVars' => ['_GET'],
     ],
     [
-        'class' => DbTarget::class,
         'logTable' => LogEntity::tableName(),
         'categories' => ['Users.Profile'],
         'levels' => ['error'],
         'logVars' => ['_POST'],
     ],
     [
-        'class' => DbTarget::class,
         'logTable' => LogEntity::tableName(),
         'categories' => ['Users.Rights'],
         'levels' => ['error'],
         'logVars' => ['_POST'],
     ],
     [
-        'class' => DbTarget::class,
         'logTable' => LogEntity::tableName(),
         'categories' => ['Users.Groups'],
         'levels' => ['error'],
         'logVars' => ['_POST'],
     ],
     [
-        'class' => DbTarget::class,
         'logTable' => LogEntity::tableName(),
         'categories' => ['Users.InitialData'],
         'levels' => ['error'],
--- src/application/modules/users/config/logs_file.php
+++ src/application/modules/users/config/logs_file.php
@@ -1,59 +1,49 @@
 <?php
 
-use yii\log\FileTarget;
-
 return [
     [
-        'class' => FileTarget::class,
         'categories' => ['Auth.Signin'],
         'levels' => ['error', 'warning'],
         'logVars' => ['_POST'],
         'logFile' => '@runtime/logs/auth/signin.log',
     ],
     [
-        'class' => FileTarget::class,
         'categories' => ['Auth.Signup'],
         'levels' => ['error', 'warning'],
         'logVars' => ['_POST'],
         'logFile' => '@runtime/logs/auth/signup.log',
     ],
     [
-        'class' => FileTarget::class,
         'categories' => ['Auth.Verification'],
         'levels' => ['error', 'warning'],
         'logVars' => ['_GET'],
         'logFile' => '@runtime/logs/auth/verification.log',
     ],
     [
-        'class' => FileTarget::class,
         'categories' => ['Auth.Recovery'],
         'levels' => ['error', 'warning'],
         'logVars' => ['_GET'],
         'logFile' => '@runtime/logs/auth/recovery.log',
     ],
     [
-        'class' => FileTarget::class,
         'categories' => ['Users.Profile'],
         'levels' => ['error', 'warning'],
         'logVars' => ['_POST'],
         'logFile' => '@runtime/logs/users/profile.log',
     ],
     [
-        'class' => FileTarget::class,
         'categories' => ['Users.Rights'],
         'levels' => ['error', 'warning'],
         'logVars' => ['_POST'],
         'logFile' => '@runtime/logs/users/rights.log',
     ],
     [
-        'class' => FileTarget::class,
         'categories' => ['Users.Groups'],
         'levels' => ['error', 'warning'],
         'logVars' => ['_POST'],
         'logFile' => '@runtime/logs/users/groups.log',
     ],
     [
-        'class' => FileTarget::class,
         'categories' => ['Users.InitialData'],
         'levels' => ['error', 'warning'],
         'logVars' => ['_POST'],
