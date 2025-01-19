57243292b13af674424256f49babd1fd77024198
stop4uk <stop4uk@yandex.ru>
2025-01-19 20:11:18 +0300

    Update menu_top.php

--- src/application/modules/users/config/menu_top.php
+++ src/application/modules/users/config/menu_top.php
@@ -1,1 +1,22 @@
 <?php
+
+use yii\bootstrap5\Html;
+
+?>
+
+<div class="navbar-collapse collapse">
+    <ul class="navbar-nav navbar-align">
+        <li class="nav-item dropdown">
+                <span class="nav-icon dropdown-toggle" role="button" data-bs-toggle="dropdown" id="personalMenu">
+                    <div class="position-relative bi bi-person-circle"></div>
+                </span>
+
+            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="personalMenu">
+                <?php
+                    echo Html::a(Html::tag('i', '', ['class' => 'align-middle me-1 bi bi-person']) . Yii::t('views', 'Профиль'), ['/profile'], ['class' => 'dropdown-item']);
+                    echo Html::a(Html::tag('i', '', ['class' => 'me-1 bi bi-box-arrow-right']) . Yii::t('views', 'Выйти'), ['/logout'], ['class' => 'dropdown-item']);
+                ?>
+            </div>
+        </li>
+    </ul>
+</div>
