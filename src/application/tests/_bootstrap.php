b15b84707752c4a8101ea9a7292a403e4f4af22a
stop4uk <stop4uk@yandex.ru>
2025-01-31 13:23:06 +0300

    Подключение и настройка Codeception

---
+++ src/application/tests/_bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+defined('YII_DEBUG') or define('YII_DEBUG', env('YII_DEBUG', true));
+defined('YII_ENV') or define('YII_ENV', env('YII_ENV', 'dev'));
+defined('YII_APP_BASE_PATH') or define('YII_APP_BASE_PATH', dirname(__FILE__, 2));
+
+require_once YII_APP_BASE_PATH . '/../vendor/autoload.php';
+require_once YII_APP_BASE_PATH . '/../vendor/yiisoft/yii2/Yii.php';
+require_once YII_APP_BASE_PATH . '/config/bootstrap.php';
