c88ef2e53ab5bfec846ebf8ce819451dde015cd5
stop4uk <stop4uk@yandex.ru>
2025-02-04 18:15:52 +0300

    Update process.php

--- src/resources/views/reports/send/process.php
+++ src/resources/views/reports/send/process.php
@@ -20,28 +20,30 @@ $this->params['breadcrumbs'] = [
 
 <div class="card">
     <div class="card-body py-0">
-        <table class="table table-borderless">
-            <tr>
-                <td class="pt-2"><?= Yii::t('views', 'Начало отчетного периода'); ?></td>
-                <td class="text-end py-0 fw-bold"><?= date('d.m.Y H:i', $model->report_datetime); ?></td>
-            </tr>
-            <tr>
-                <td class="py-0"><?= Yii::t('views', 'Группа'); ?></td>
-                <td class="text-end py-0 fw-bold"><?= $model->group->name; ?></td>
-            </tr>
-            <tr>
-                <td class="py-0"><?= Yii::t('views', 'Пользователь'); ?></td>
-                <td class="text-end py-0 fw-bold"><?= Yii::$app->getUser()->getIdentity()->shortName; ?></td>
-            </tr>
-
-            <?php if ($model->form_control ): ?>
+        <div class="table-responsive">
+            <table class="table table-borderless">
                 <tr>
-                    <td colspan="2" class="py-0 text-center h3">
-                        <?= Html::tag('code', Yii::t('views', 'Режим контроля')); ?>
-                    </td>
+                    <td class="pt-2" style="min-width: 14rem"><?= Yii::t('views', 'Начало отчетного периода'); ?></td>
+                    <td class="text-end py-0 fw-bold" style="min-width: 12rem"><?= date('d.m.Y H:i', $model->report_datetime); ?></td>
                 </tr>
-            <?php endif; ?>
-        </table>
+                <tr>
+                    <td class="py-0" style="min-width: 14rem"><?= Yii::t('views', 'Группа'); ?></td>
+                    <td class="text-end py-0 fw-bold" style="min-width: 12rem"><?= $model->group->name; ?></td>
+                </tr>
+                <tr>
+                    <td class="py-0" style="min-width: 14rem"><?= Yii::t('views', 'Пользователь'); ?></td>
+                    <td class="text-end py-0 fw-bold"><?= Yii::$app->getUser()->getIdentity()->shortName; ?></td>
+                </tr>
+
+                <?php if ($model->form_control ): ?>
+                    <tr>
+                        <td colspan="2" class="py-0 text-center h3">
+                            <?= Html::tag('code', Yii::t('views', 'Режим контроля')); ?>
+                        </td>
+                    </tr>
+                <?php endif; ?>
+            </table>
+        </div>
     </div>
 </div>
 <div class="card mt-3">
