bc518e1528f328e2745cab9a627567e684d9bc4c
stop4uk <stop4uk@yandex.ru>
2025-02-06 12:53:21 +0300

    Update m240410_104237_groups.php

--- src/application/modules/users/migrations/m240410_104237_groups.php
+++ src/application/modules/users/migrations/m240410_104237_groups.php
@@ -4,7 +4,7 @@ namespace app\modules\users\migrations;
 
 use yii\db\Migration;
 
-final class m240410_104237_groups extends Migration
+class m240410_104237_groups extends Migration
 {
     const TABLE = '{{%groups}}';
 
