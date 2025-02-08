744a8846f5c72c41d950e037abe6b7a17572b23d
stop4uk <stop4uk@yandex.ru>
2025-02-08 11:32:46 +0300

    Перенос тестов в корневой раздел

---
+++ src/tests/_bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+define('YII_DEBUG', true);
+define('YII_ENV', 'dev');
+define('YII_APP_BASE_PATH', dirname(__FILE__, 2));
+
+require_once YII_APP_BASE_PATH . '/vendor/autoload.php';
+require_once YII_APP_BASE_PATH . '/vendor/yiisoft/yii2/Yii.php';
+require_once YII_APP_BASE_PATH . '/application/config/bootstrap.php';
