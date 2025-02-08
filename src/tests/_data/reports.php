bc8e45af2716191e5316bc7fefbec1fd18ec26b0
stop4uk <stop4uk@yandex.ru>
2025-02-09 01:34:01 +0300

    Наполнение тестовых данных

--- src/tests/_data/reports.php
+++ src/tests/_data/reports.php
@@ -1,5 +1,22 @@
 <?php
 
 return [
-
+    [
+        'id' => 1,
+        'name' => 'Тестовый отчет',
+        'created_at' => time(),
+        'created_uid' => 1,
+        'created_gid' => 1
+    ],
+    [
+        'id' => 2,
+        'name' => 'Тестовый отчет2',
+        'description' => '"<p>Тестовое описание отчета 2</p>"',
+        'left_period' => 1440,
+        'block_minutes' => 30,
+        'null_day' => 1,
+        'created_at' => time(),
+        'created_uid' => 1,
+        'created_gid' => 1
+    ],
 ];
--- src/tests/_data/reportsFormTemplates.php
+++ src/tests/_data/reportsFormTemplates.php
@@ -1,5 +1,32 @@
 <?php
 
-return [
+use app\helpers\CommonHelper;
 
+return [
+    [
+        'id' => 1,
+        'report_id' => 1,
+        'name' => 'Шаблон 1',
+        'form_datetime' => 2,
+        'form_type' => 1,
+        'form_usejobs' => 1,
+        'use_appg' => 0,
+        'created_at' => time(),
+        'created_uid' => 1,
+        'created_gid' => 1,
+    ],
+    [
+        'id' => 2,
+        'report_id' => 2,
+        'name' => 'Шаблон 2',
+        'form_datetime' => 2,
+        'form_type' => 0,
+        'form_usejobs' => 0,
+        'use_appg' => 0,
+        'table_type' => 0,
+        'table_columns' => CommonHelper::implodeField(['record1', 'record2', 'record5']),
+        'created_at' => time(),
+        'created_uid' => 1,
+        'created_gid' => 1,
+    ],
 ];
