448b65dcc1db75851c45fcbdad65662afecf2d06
stop4uk <stop4uk@yandex.ru>
2025-02-10 15:05:04 +0300

    Заполнение тестовых данных

--- src/tests/_data/reportsDataChanges.php
+++ src/tests/_data/reportsDataChanges.php
@@ -1,5 +1,13 @@
 <?php
 
 return [
-
+    [
+        'id' => 1,
+        'report_id' => 2,
+        'data_id' => 4,
+        'content' => '{"record2":{"operation":1,"value":"22","oldValue":"2"}}',
+        'created_at' => time(),
+        'created_uid' => 1,
+        'created_gid' => 1
+    ]
 ];
