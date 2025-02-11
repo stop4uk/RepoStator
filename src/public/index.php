af13f393efb407443019b9074ae1dc5573943637
stop4uk <stop4uk@yandex.ru>
2025-02-11 16:31:16 +0300

    Update index.php

--- src/public/index.php
+++ src/public/index.php
@@ -1,7 +1,7 @@
 <?php
 
-define('YII_DEBUG', getenv('YII_DEBUG'));
-define('YII_ENV', getenv('YII_ENV'));
+defined('YII_DEBUG') or define('YII_DEBUG', env('YII_DEBUG', true));
+defined('YII_ENV') or define('YII_ENV', env('YII_ENV', 'dev'));
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
