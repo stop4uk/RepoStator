08c58f0d2b304e8e4c8bc8d1d1ff93bfc13a432e
stop4uk <stop4uk@yandex.ru>
2025-02-11 21:25:22 +0300

    Update index-test.php

--- src/public/index-test.php
+++ src/public/index-test.php
@@ -1,7 +1,7 @@
 <?php
 
-define('YII_DEBUG', true);
-define('YII_ENV', 'test');
+defined('YII_DEBUG') or define('YII_DEBUG', getenv('YII_DEBUG', true));
+defined('YII_ENV') or define('YII_ENV', getenv('YII_ENV', 'test'));
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
