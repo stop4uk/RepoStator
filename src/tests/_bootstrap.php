d3efd8dc78b0c19c1f067669f91784b531fdc083
stop4uk <stop4uk@yandex.ru>
2025-02-11 15:38:54 +0300

    Тестовое окружение и настройки

--- src/tests/_bootstrap.php
+++ src/tests/_bootstrap.php
@@ -1,9 +1,8 @@
 <?php
 
+define('YII_ENV', 'test');
 define('YII_DEBUG', true);
-define('YII_ENV', 'dev');
 define('YII_APP_BASE_PATH', dirname(__FILE__, 2));
 
 require_once YII_APP_BASE_PATH . '/vendor/autoload.php';
 require_once YII_APP_BASE_PATH . '/vendor/yiisoft/yii2/Yii.php';
-require_once YII_APP_BASE_PATH . '/application/config/bootstrap.php';
