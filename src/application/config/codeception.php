47820c3b47427a3f7c4290359b9ade3cd8474fb5
stop4uk <stop4uk@yandex.ru>
2025-02-11 21:25:16 +0300

    Update codeception.php

--- src/application/config/codeception.php
+++ src/application/config/codeception.php
@@ -5,7 +5,6 @@ use app\components\attachedFiles\AttachFileHelper;
 
 return [
     'id' => 'repostator-test',
-    'defaultRoute' => 'reports/dashboard',
     'components' => [
         AttachFileHelper::STORAGE_LOCAL => [
             'class' => LocalFilesystem::class,
