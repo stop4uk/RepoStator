4a0e62a9658a88a9a45801c71135452c8dd8d9db
stop4uk <stop4uk@yandex.ru>
2025-02-11 15:48:02 +0300

    Update groupsNested.php
    Фикс данных. Исправление ID

--- src/tests/_data/groupsNested.php
+++ src/tests/_data/groupsNested.php
@@ -9,21 +9,21 @@ return [
         'depth' => 0
     ],
     [
-        'id' => 1,
+        'id' => 2,
         'group_id' => 2,
         'lft' => 1,
         'rgt' => 2,
         'depth' => 1
     ],
     [
-        'id' => 1,
+        'id' => 3,
         'group_id' => 3,
         'lft' => 3,
         'rgt' => 4,
         'depth' => 1
     ],
     [
-        'id' => 1,
+        'id' => 4,
         'group_id' => 4,
         'lft' => 5,
         'rgt' => 6,
