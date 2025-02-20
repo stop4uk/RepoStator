057b3cbef4894c01b58c1a24f45e61b51685c0de
stop4uk <stop4uk@yandex.ru>
2025-02-20 11:23:57 +0300

    Update edit.php

--- src/resources/views/reports/default/edit.php
+++ src/resources/views/reports/default/edit.php
@@ -20,17 +20,17 @@ $this->params['breadcrumbs'] = [
 <div class="d-grid d-md-flex justify-content-md-end gap-2 gap-md-0 mb-2">
     <?php
         $ruleArray = $model->getEntity()->toArray(['created_uid', 'created_gid', 'record_status']);
-        $rolesArray = [
-            'report.delete.main',
-            'report.delete.group',
-            'report.delete.all',
-        ];
+        $rolesArray = ['report.delete.main', 'report.delete.group', 'report.delete.all',];
 
-        if ($model->getEntity()->record_status && RbacHelper::canArray($rolesArray, $ruleArray)) {
+        if (
+                $model->getEntity()->record_status
+                && RbacHelper::canArray($rolesArray, $ruleArray)
+        ) {
             echo Html::a(
                 Yii::t('views', 'Удалить отчет'),
                 Url::to(['delete', 'id' => $model->getEntity()->id, 'fromEdit' => true]),
                 [
+                    'id' => "deleteButton_{$model->getEntity()->id}",
                     'class' => 'btn btn-danger',
                     'data' => [
                         'confirm' => Yii::t('views', 'Вы действительно хотите удалить отчет "{name}"?', ['name' =>  $model->name]),
