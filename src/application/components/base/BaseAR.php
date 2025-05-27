f5d3b3ba009a7b82b7aa70f4a7dbb4a1f62ae83b
stop4uk <stop4uk@yandex.ru>
2025-05-27 11:22:31 +0300

    Update BaseAR.php
    Redeclare save method

--- src/application/components/base/BaseAR.php
+++ src/application/components/base/BaseAR.php
@@ -2,6 +2,8 @@
 
 namespace app\components\base;
 
+use Yii;
+use yii\base\Exception;
 use yii\db\ActiveRecord;
 
 /**
@@ -12,14 +14,33 @@ class BaseAR extends ActiveRecord implements BaseARInterface
 {
     const RSTATUS_DELETED = 0;
     const RSTATUS_ACTIVE = 1;
+    public const RSTATUSES = [
+        self::RSTATUS_DELETED,
+        self::RSTATUS_ACTIVE
+    ];
 
     const SCENARIO_INSERT = 'insert';
     const SCENARIO_UPDATE = 'update';
     const SCENARIO_DELETE = 'delete';
     const SCENARIO_CHANGE_RECORD_STATUS = 'changeRecordStatus';
 
-    public const RSTATUSES = [
-        self::RSTATUS_DELETED,
-        self::RSTATUS_ACTIVE
-    ];
+    public function save(
+        $runValidation = true,
+        $attributeNames = null,
+        string|null $logCategory = null
+    ): bool {
+        if (!$this->validate($attributeNames)) {
+            Yii::error($this->getErrors(), $logCategory);
+            return false;
+        }
+
+        try {
+            parent::save($runValidation, $attributeNames);
+        } catch (Exception $exception) {
+            Yii::error($exception->getMessage(), $logCategory);
+            return false;
+        }
+
+        return false;
+    }
 }
